f23ba4937c23e24dca6e804ca952f1df682699a4
Goodnessuc <ukejegoodness599@gmail.com>
2022-08-06 14:09:40 +0100

    changes

---
+++ graph/model/BioRepo.go
@@ -0,0 +1,48 @@
+package model
+
+import "gorm.io/gorm"
+
+type BookRepository interface {
+	CreatePerson(bioData BioData) (BioData, error)
+	UpdatePerson(bioData BioData, id int) error
+	DeletePerson(id int) error
+	GetAllPersons() ([]BioData, error)
+}
+
+type BioService struct {
+	db *gorm.DB
+}
+
+func (bio BioService) CreatePerson(bioData BioData) (BioData, error) {
+	bioInstance := BioData{
+		ID:   bioData.ID,
+		Name: bioData.Name,
+		Age:  bioData.Age,
+	}
+
+	err := bio.db.Create(&bioInstance).Error
+	return bioInstance, err
+}
+
+func (bio BioService) UpdatePerson(bioData BioData, id int) error {
+	bioInstance := BioData{
+		ID:   bioData.ID,
+		Name: bioData.Name,
+		Age:  bioData.Age,
+	}
+	err := bio.db.Model(&bioData).Where("id = ?", id).Updates(bioInstance).Error
+	return err
+}
+
+func (bio BioService) DeletePerson(id int) error {
+	book := BioData{}
+	err := bio.db.Delete(book, id).Error
+	return err
+}
+
+func (bio BioService) GetAllPersons() ([]BioData, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+var _ BookRepository = &BioService{}
